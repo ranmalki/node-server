0ad64f5cb7015e362bdc01e2e9ac434a37db2503
ranmalki <86737634+ranmalki@users.noreply.github.com>
2021-08-11 16:48:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const http = require('http');
+const fs = require('fs');
 
+const notFound = './404.html'
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end(createNav() + '<h1>Home Page</h1>');
+        return;
+    } else if (req.url === '/contact') {
+        res.end(createNav() + '<p>Contact Me</p>');
+        return;
+    } else if(req.url === '/about') {
+        res.end(createNav() + '<p>About Me</p>');
+        return;
+    }
+    const content = fs.readFileSync(notFound);
+    res.statusCode = 404;
+    res.end(content);
+    return;
+})
+
+server.listen(8080, () => {
+    console.log('listening to 8080');
+})
+
+function createNav() {
+    return '<div><ul><li><a href="http://localhost:8080/">Home Page</a></li><li><a href="http://localhost:8080/about">About</a></li><li><a href="http://localhost:8080/contact">Contact Me</a></li></ul></div>'
+}
